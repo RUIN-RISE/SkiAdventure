a684b40efa96b7946e18aab5bb6ec27788f7780d
sugarr <1239626175@qq.com>
2025-07-02 19:17:46 +0800

    初步完成App类框架

---
+++ code/app/app.cpp
@@ -0,0 +1,19 @@
+#include "App.h"
+
+App::App() : m_main_wnd(600, 700, "Aircraft Battle") {}
+
+bool App::initialize(){
+
+	//binding
+
+	//properties
+	
+
+	//commands
+	
+
+	//notification
+	
+
+	return true;
+}
--- code/app/app.h
+++ code/app/app.h
@@ -1,5 +1,25 @@
+#include "../viewmodel/GameViewModel.h"
+
 #include "../view/MainWindow.h"
 
 class App {
+public:
+	App();
+	App(const App&) = delete;
+	~App() noexcept
+	{
+	}
 
-}
+	App& operator=(const App&) = delete;
+
+	bool initialize();
+
+	void show_main_window()
+	{
+		m_main_wnd.show();
+	}
+
+private:
+	MainWindow m_main_wnd;
+	GameViewModel m_game_viewmodel;
+};
